1ca63f078756c1e47d61171a46b3b6202709f8bb
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2019-08-09 02:56:02 +0200

    added all AST constructs

--- src/Language/PureScript/CodeGen/JS/Printer.hs
+++ src/Language/PureScript/CodeGen/JS/Printer.hs
@@ -26,6 +26,7 @@ import Language.PureScript.Crash
 import Language.PureScript.Pretty.Common (SMap)
 import Language.PureScript.PSString (PSString, decodeString, prettyPrintStringJS)
 
+nest' :: Doc () -> Doc ()
 nest' = nest 2
 
 -- | Generate a pretty-printed string representing a collection of JavaScript expressions at the same indentation level
@@ -35,39 +36,98 @@ prettyPrintJSWithSourceMaps js = ("asd", [])
   -- in (s, mp)
 
 prettyPrintUnary :: UnaryOperator -> Doc ()
+prettyPrintUnary Negate = "-"
 prettyPrintUnary Not = "!"
+prettyPrintUnary BitwiseNot = "~"
+prettyPrintUnary Positive = "+"
 prettyPrintUnary New = "new "
 
+prettyPrintBinary :: BinaryOperator -> Doc ()
+prettyPrintBinary Multiply             = "*"
+prettyPrintBinary Divide               = "/"
+prettyPrintBinary Modulus              = "%"
+prettyPrintBinary Add                  = "+"
+prettyPrintBinary Subtract             = "-"
+prettyPrintBinary ShiftLeft            = "<<"
+prettyPrintBinary ShiftRight           = ">>"
+prettyPrintBinary ZeroFillShiftRight   = ">>>"
+prettyPrintBinary LessThan             = "<"
+prettyPrintBinary LessThanOrEqualTo    = "<="
+prettyPrintBinary GreaterThan          = ">"
+prettyPrintBinary GreaterThanOrEqualTo = ">="
+prettyPrintBinary EqualTo              = "==="
+prettyPrintBinary NotEqualTo           = "!=="
+prettyPrintBinary BitwiseAnd           = "&"
+prettyPrintBinary BitwiseXor           = "^"
+prettyPrintBinary BitwiseOr            = "|"
+prettyPrintBinary And                  = "&&"
+prettyPrintBinary Or                   = "||"
+
+sepComma :: [Doc ()] -> Doc ()
+sepComma = concatWith (\a b -> a <> "," <> line <> b)
+
 prettyPrintAst :: AST -> Doc ()
-prettyPrintAst (VariableIntroduction sm name Nothing) = pretty name
-prettyPrintAst (VariableIntroduction sm name (Just val)) = "const" <+> pretty name <+> "=" <+> prettyPrintAst val
-prettyPrintAst (NumericLiteral sm (Right val)) = pretty val
-prettyPrintAst (NumericLiteral sm (Left val)) = pretty val
-prettyPrintAst (Function sm name args block) = vsep
-  [ nest' $ "function" <+> (maybe mempty ((<> space) . pretty) name) <> parens (sep $ pretty <$> args) <+> "{" <> line <>
+prettyPrintAst (VariableIntroduction _ name Nothing) = pretty name
+prettyPrintAst (VariableIntroduction _ name (Just val)) = "const" <+> pretty name <+> "=" <+> prettyPrintAst val
+prettyPrintAst (NumericLiteral _ (Right val)) = pretty val
+prettyPrintAst (NumericLiteral _ (Left val)) = pretty val
+prettyPrintAst (Function _ name args block) = vsep
+  [ nest' $ "function" <+> (maybe mempty ((<> space) . pretty) name) <> parens (group $ sepComma $ pretty <$> args) <+> "{" <> line <>
     (prettyPrintAst block)
   , "}"
   ]
-prettyPrintAst (App sm val args) = prettyPrintAst val <> nest' (line <> parens (concatWith (\a b -> a <> "," <> line <> b) $ prettyPrintAst <$> args))
-prettyPrintAst (Indexer sm a b) = prettyPrintAst b <> brackets (prettyPrintAst a)
-prettyPrintAst (StringLiteral sm str) = pretty $ prettyPrintStringJS str
-prettyPrintAst (ObjectLiteral sm elements) = vsep
+prettyPrintAst (App _ app args) = printFunc app <> (nest' (line' <> parens (group $ sepComma $ prettyPrintAst <$> args)))
+    where
+      printFunc val@(Function _ _ _ _) = parens (prettyPrintAst val)
+      printFunc val = prettyPrintAst val
+prettyPrintAst (Indexer _ a b) = prettyPrintAst b <> brackets (prettyPrintAst a)
+prettyPrintAst (StringLiteral _ str) = pretty $ prettyPrintStringJS str
+prettyPrintAst (ObjectLiteral _ elements) = vsep
   [ nest' $ "{" <> line <> 
-      group (concatWith (\a b -> a <> "," <> line <> b) (map go elements))
+      group (sepComma (map go elements))
   , "}"
   ]
   where
     go (str, val) = (pretty $ prettyPrintStringJS str) <> ":" <+> prettyPrintAst val
-prettyPrintAst (Assignment sm var val) = prettyPrintAst var <+> "=" <+> prettyPrintAst val
-prettyPrintAst (Unary sm op expr) = prettyPrintUnary op <> prettyPrintAst expr
--- prettyPrintAst (Binary sm op a b) = prettyPrintUnary <> prettyprintAst expr
-prettyPrintAst (Var sm name) = pretty name
-prettyPrintAst (Block sm sts) = prettyPrintSts sts
-prettyPrintAst (Return sm expr) = nest' $ "return" <> softline <> prettyPrintAst expr
-prettyPrintAst s = pretty $ show s
+prettyPrintAst (Assignment _ var val) = prettyPrintAst var <+> "=" <+> prettyPrintAst val
+prettyPrintAst (Unary _ op expr) = prettyPrintUnary op <> prettyPrintAst expr
+prettyPrintAst (Binary _ op a b) = prettyPrintAst a <> prettyPrintBinary op <> prettyPrintAst b
+prettyPrintAst (Var _ name) = pretty name
+prettyPrintAst (Block _ sts) = prettyPrintSts sts
+prettyPrintAst (Return _ expr) = nest' $ "return" <> softline <> prettyPrintAst expr
+prettyPrintAst (BooleanLiteral _ True) = "true"
+prettyPrintAst (BooleanLiteral _ False) = "false"
+prettyPrintAst (ArrayLiteral _ expr) = list $ map prettyPrintAst expr
+prettyPrintAst (While _ cond expr) = "while" <+> parens (prettyPrintAst cond) <+> prettyPrintAst expr
+prettyPrintAst (For _ ident start end sts) = "for (var" 
+  <> pretty ident <+> "=" <+> prettyPrintAst start <> ";" 
+  <+> pretty ident <+> "<" <+> prettyPrintAst end <> ";"
+  <+> pretty ident <> "++"
+  <+> prettyPrintAst sts
+prettyPrintAst (ForIn _ ident obj sts) = "for (var" <+> pretty ident <+> "in" <+> prettyPrintAst obj <> ")" <+> prettyPrintAst sts
+prettyPrintAst (IfElse _ cond thens elses) = "if" <+> parens (prettyPrintAst cond) <+> prettyPrintAst thens <> prettyElse elses
+    where 
+      prettyElse (Just s) = " else" <+> prettyPrintAst s
+      prettyElse Nothing = ""
+prettyPrintAst (ReturnNoResult _) = "return"
+prettyPrintAst (Throw _ value) = "throw" <+> prettyPrintAst value
+prettyPrintAst (Comment _ com ast) = sep (map prettyPrintComment com) <> prettyPrintAst ast
+prettyPrintAst (InstanceOf _ a b) = prettyPrintAst a <+> "instanceof" <+> prettyPrintAst b
+
+prettyPrintComment :: Comment -> Doc ()
+prettyPrintComment (LineComment com) = "//" <+> pretty com <> line
+prettyPrintComment (BlockComment com) = "/*" <+> pretty com <+> "*/"
+
+-- prettyPrintAst s = pretty $ show s
 
 prettyPrintSts :: [AST] -> Doc ()
 prettyPrintSts sts = cat $ zipWith (<>) (prettyPrintAst <$> sts) (repeat ";")
 
+prettyPrintStsTopLevel :: [AST] -> Doc ()
+prettyPrintStsTopLevel sts = cat $ zipWith (<>) (prettyPrintAst <$> sts) (repeat $ ";"<>line')
+
+layoutOptions :: LayoutOptions
+layoutOptions = LayoutOptions {layoutPageWidth = AvailablePerLine 120 0.8 }
+
 prettyPrintJS :: [AST] -> Text
-prettyPrintJS ast = renderStrict $ layoutPretty defaultLayoutOptions $ prettyPrintSts ast -- maybe (internalError "Incomplete pattern") runPlainString . flip evalStateT (PrinterState 0) . prettyStatements
+prettyPrintJS ast = renderStrict $ layoutPretty layoutOptions $ prettyPrintStsTopLevel ast -- maybe (internalError "Incomplete pattern") runPlainString . flip evalStateT (PrinterState 0) . prettyStatements
